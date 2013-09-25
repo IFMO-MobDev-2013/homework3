b4054b547621b4ee6b0fd83f79ed91fc8ebbc37f
h0tk3y <hotkeytlt@gmail.com>
2013-09-25 20:09:48 +0400

    + InputActivity.java

---
+++ src/com/ifmomd/slate/InputActivity.java
@@ -0,0 +1,13 @@
+package com.ifmomd.slate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InputActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
