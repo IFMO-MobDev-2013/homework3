a94b00d36967d311b7993ca3ea0dc20b23c6b076
georgeee <george.agapov@gmail.com>
2013-10-03 15:48:08 +0400

    Added closestWidth method

--- SilencioTest/src/ru/georgeee/android/Silencio/ImageApiTest.java
+++ SilencioTest/src/ru/georgeee/android/Silencio/ImageApiTest.java
@@ -1,14 +1,10 @@
 package ru.georgeee.android.Silencio;
 
-import android.content.Context;
-import android.test.InstrumentationTestCase;
+import android.test.ActivityInstrumentationTestCase2;
 import android.util.Log;
-import ru.georgeee.android.Silencio.utility.cacher.FileCacher;
-import ru.georgeee.android.Silencio.utility.http.HttpUtility;
 import ru.georgeee.android.Silencio.utility.http.image.ImageApiResult;
+import ru.georgeee.android.Silencio.utility.http.image.flickr.FlickrImageApiResult;
 import ru.georgeee.android.Silencio.utility.http.image.flickr.FlickrImageApiTask;
-
-import java.util.Arrays;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,14 +13,26 @@ import java.util.Arrays;
  * Time: 23:00
  * To change this template use File | Settings | File Templates.
  */
-public class ImageApiTest extends InstrumentationTestCase {
+public class ImageApiTest extends ActivityInstrumentationTestCase2<SearchActivity> {
+
+    public ImageApiTest() {
+        super("ru.georgeee.android.Silencio", SearchActivity.class);
+    }
     public static final String API_KEY = "c6e9f7d17d4a23a08281d34b482a2785";
 
     public void testImageApi() throws Exception {
-        ImageApiResult result = new FlickrImageApiTask(API_KEY, "Магазинчик БО").executeOnHttpTaskExecutor().get();
+        FlickrImageApiResult result = (FlickrImageApiResult)new FlickrImageApiTask(API_KEY, "Магазинчик БО").executeOnHttpTaskExecutor().get();
 
-        Log.d("ImageApiTest", Arrays.toString(result.getImages()));
-
+        for(final ImageApiResult.Image _image: result.getImages()){
+            final FlickrImageApiResult.FlickrImage image = (FlickrImageApiResult.FlickrImage)_image;
+            image.asyncLoadSizes(new FlickrImageApiResult.FlickrImage.SizeLoadedHandler() {
+                @Override
+                protected void onPostExecute(FlickrImageApiResult.FlickrImage.Size[] sizes) {
+                    FlickrImageApiResult.FlickrImage.Size closestSize = image.getSizeWithClosestWidth(1000);
+                    Log.d("ImageApiTest", "Width:1000 closest: " + closestSize.getWidth() + " " + closestSize.getSource());
+                }
+            }, API_KEY);
+        }
         assertTrue(result.getImages().length > 0);
 
     }
