47b8c88063f81c3214439844c2dc32cc0b6ae5cb
Koshik <438@nm.ru>
2013-10-03 20:42:22 +0600

    commit

---
+++ src/com/example/task3/SecondActivity.java
@@ -0,0 +1,26 @@
+package com.example.task3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class SecondActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pictures);
+        TextView translation = (TextView) findViewById(R.id.textview);
+        String text = getIntent().getStringExtra("translation");
+        translation.setText(text);
+
+    }
+
+    public void changeActivity(View v) {
+        Intent i = new Intent(SecondActivity.this, MyActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
