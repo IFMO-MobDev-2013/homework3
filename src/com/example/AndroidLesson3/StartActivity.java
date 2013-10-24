d0ce3d175b9e50db7b2cc464e8a99ffd485f5d3c
Ilya Isaev <isaev@loyaltyplant.com>
2013-10-25 01:02:27 +0400

    async task

--- src/com/example/AndroidLesson3/StartActivity.java
+++ src/com/example/AndroidLesson3/StartActivity.java
@@ -2,11 +2,13 @@ package com.example.AndroidLesson3;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
@@ -14,16 +16,19 @@ import org.xml.sax.InputSource;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 
 public class StartActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
-    public static final String apiKey = "trnsl.1.1.20130925T205049Z.a92cd36d99706af9.b5a1aaf3c0f791ceb482029c486691897db34357";
-    public static final String yandexUrl ="https://translate.yandex.net/api/v1.5/tr/translate";
-    Translator translator = new Translator();
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,29 +36,19 @@ public class StartActivity extends Activity {
         setContentView(R.layout.main);
         Button submit = (Button) findViewById(R.id.button);
         final Intent intent = new Intent(this, TranslateActivity.class);
+        final Activity activity = this;
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
                     CharSequence sequence = ((EditText) findViewById(R.id.editText)).getText();
-                    String translate = getYandexTranslate(sequence);
-                    intent.putExtra("translate",translate);
+                    intent.putExtra("translate",sequence);
                 } catch (Exception e) {
-                    Log.e("lol", "lol", e);
+                    Log.e("bugbug", "bugbug", e);
                 }
                 startActivity(intent);
             }
         });
-    }
-
-
-
-    private String getYandexTranslate(CharSequence sequence) throws ParserConfigurationException, InterruptedException {
-        HashMap<String,String>parameters = new HashMap<>();
-        parameters.put("key",apiKey);
-        parameters.put("text",sequence.toString());
-        parameters.put("lang","ru");
-        return translator.translate(sequence.toString(), yandexUrl,parameters);
     }
 
 }
--- src/com/example/AndroidLesson3/TranslateActivity.java
+++ src/com/example/AndroidLesson3/TranslateActivity.java
@@ -1,8 +1,25 @@
 package com.example.AndroidLesson3;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,10 +29,60 @@ import android.widget.TextView;
  * To change this template use File | Settings | File Templates.
  */
 public class TranslateActivity extends Activity {
+
+    class Translator extends AsyncTask<String, Void, String> {
+        public Activity fromActivity;
+        protected String address;
+        public static final String apiKey = "trnsl.1.1.20130925T205049Z.a92cd36d99706af9.b5a1aaf3c0f791ceb482029c486691897db34357";
+        public static final String yandexUrl = "https://translate.yandex.net/api/v1.5/tr/translate";
+
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder sb = new StringBuilder();
+            String result;
+            try {
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = factory.newDocumentBuilder();
+                Document doc;
+                address = yandexUrl + "?key=" + apiKey + "&text=" + URLEncoder.encode(params[0],"UTF-8") + "&lang=ru";
+                URL url = new URL(address);
+                HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+                BufferedReader in = new BufferedReader(new InputStreamReader(new BufferedInputStream(connect.getInputStream())));
+                String temp;
+                while ((temp = in.readLine()) != null) {
+                    sb.append(temp);
+                }
+
+                String translationXml = sb.toString();
+                InputSource is = new InputSource();
+                is.setCharacterStream(new StringReader(sb.toString()));
+                doc = db.parse(is);
+                if (translationXml == null) {
+                    result = "no internet connection";
+                } else {
+                    result = doc.getElementsByTagName("text").item(0).getTextContent();
+                }
+            } catch (Exception e) {
+                result = "internal error";
+                Log.e("lol", "lol", e);
+            }
+            return result;
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView translationView = (TextView) findViewById(R.id.translate);
+            translationView.setText(result);
+        }
+    }
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.translate);
         TextView name = (TextView) findViewById(R.id.translate);
-        name.setText(getIntent().getExtras().getCharSequence("translate"));
+        CharSequence sequence = getIntent().getExtras().getCharSequence("translate");
+        new Translator().execute(sequence.toString());
+        name.setText(sequence);
     }
 }
