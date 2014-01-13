283e0bb9f8028c2a9af53844ffd531a25b0b1e4b
gelkin <vladimir@vladimir-Lenovo-G570.(none)>
2014-01-13 20:39:06 +0400

    Images added

--- src/com/example/WebSearch/TranslationActivity.java
+++ src/com/example/WebSearch/TranslationActivity.java
@@ -53,8 +53,17 @@ public class TranslationActivity extends Activity {
                 result = "NO TRANSLATION. SORRY";
             }
             textOut.setText(text + "  ->(ru) " + result);
-            
-            // TODO
+            /*
+            findViewById(R.id.showImagesButton).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), ShowImagesActivity.class);
+                    intent.putExtra("enText", text);
+                    System.out.println("TRRRASNSLATION:::::: " + text);
+                    startActivity(intent);
+                }
+            });
+            */
         }
     }
 }
